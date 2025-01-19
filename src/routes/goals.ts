61313031cbdee5875c257b8a8842c6662c56bb9f
Jakob Jonasson <jakob.jonasson@gmail.com>
2025-01-19 20:37:22 +0000

    Add mock data

--- src/routes/goals.ts
+++ src/routes/goals.ts
@@ -1,8 +1,42 @@
 import { Hono } from "hono";
 
+type Goal = {
+    id: string;
+    title: string;
+    date: string;
+    description: string;
+    completed: boolean;
+};
+
+const getMockGoals = (): Goal[] => {
+    return [
+        {
+            id: "1",
+            title: "Goal 1",
+            date: "2021-01-01",
+            description: "This is the first goal",
+            completed: false,
+        },
+        {
+            id: "2",
+            title: "Goal 2",
+            date: "2021-01-02",
+            description: "This is the second goal",
+            completed: false,
+        },
+        {
+            id: "3",
+            title: "Goal 3",
+            date: "2021-01-03",
+            description: "This is the third goal",
+            completed: false,
+        },
+    ];
+};
+
 export const goalsRoute = new Hono()
     .get("/", (c) => {
-        return c.json({ goals: [] });
+        return c.json({ goals: getMockGoals() });
     })
     .post("/", (c) => {
         return c.json({});
