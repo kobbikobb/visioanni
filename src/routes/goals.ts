9ca60c2b84564e5a4598ee65f22616211fdace97
Jakob Jonasson <jakob.jonasson@gmail.com>
2025-01-19 22:06:11 +0000

    Update prettier

--- src/routes/goals.ts
+++ src/routes/goals.ts
@@ -1,4 +1,4 @@
-import { Hono } from "hono";
+import { Hono } from 'hono';
 
 type Goal = {
     id: string;
@@ -8,36 +8,40 @@ type Goal = {
     completed: boolean;
 };
 
+const mockGoals = [
+    {
+        id: '1',
+        title: 'Goal 1',
+        date: '2021-01-01',
+        description: 'This is the first goal',
+        completed: false
+    },
+    {
+        id: '2',
+        title: 'Goal 2',
+        date: '2021-01-02',
+        description: 'This is the second goal',
+        completed: false
+    },
+    {
+        id: '3',
+        title: 'Goal 3',
+        date: '2021-01-03',
+        description: 'This is the third goal',
+        completed: false
+    }
+];
+
 const getMockGoals = (): Goal[] => {
-    return [
-        {
-            id: "1",
-            title: "Goal 1",
-            date: "2021-01-01",
-            description: "This is the first goal",
-            completed: false,
-        },
-        {
-            id: "2",
-            title: "Goal 2",
-            date: "2021-01-02",
-            description: "This is the second goal",
-            completed: false,
-        },
-        {
-            id: "3",
-            title: "Goal 3",
-            date: "2021-01-03",
-            description: "This is the third goal",
-            completed: false,
-        },
-    ];
+    return mockGoals;
 };
 
 export const goalsRoute = new Hono()
-    .get("/", (c) => {
+    .get('/', (c) => {
         return c.json({ goals: getMockGoals() });
     })
-    .post("/", (c) => {
-        return c.json({});
+    .post('/', async (c) => {
+        const goal = (await c.req.json()) as Goal;
+        mockGoals.push(goal);
+        return c.json({ goal });
     });
