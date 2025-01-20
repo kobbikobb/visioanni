1df8c3c8b2de41cd26421f457ec8b30b01654912
Jakob Jonasson <jakob.jonasson@gmail.com>
2025-01-20 08:59:10 +0000

    Validate Goals

--- src/routes/goals.ts
+++ src/routes/goals.ts
@@ -1,7 +1,9 @@
 import { Hono } from 'hono';
+import { z } from 'zod';
+import { zValidator } from '@hono/zod-validator';
 
 type Goal = {
-    id: string;
+    id: number;
     title: string;
     date: string;
     description: string;
@@ -10,21 +12,21 @@ type Goal = {
 
 const mockGoals = [
     {
-        id: '1',
+        id: 1,
         title: 'Goal 1',
         date: '2021-01-01',
         description: 'This is the first goal',
         completed: false
     },
     {
-        id: '2',
+        id: 2,
         title: 'Goal 2',
         date: '2021-01-02',
         description: 'This is the second goal',
         completed: false
     },
     {
-        id: '3',
+        id: 3,
         title: 'Goal 3',
         date: '2021-01-03',
         description: 'This is the third goal',
@@ -36,12 +38,22 @@ const getMockGoals = (): Goal[] => {
     return mockGoals;
 };
 
+const goalPostSchema = z.object({
+    title: z.string().min(1),
+    date: z
+        .string()
+        .regex(/^\d{4}-\d{2}-\d{2}$/, 'Date must be in the format yyyy-MM-dd'),
+    description: z.string(),
+    completed: z.boolean()
+});
+
 export const goalsRoute = new Hono()
     .get('/', (c) => {
         return c.json({ goals: getMockGoals() });
     })
-    .post('/', async (c) => {
-        const goal = (await c.req.json()) as Goal;
+    .post('/', zValidator('json', goalPostSchema), async (c) => {
+        const goalInput = c.req.valid('json');
+        const goal: Goal = { id: mockGoals.length + 1, ...goalInput };
         mockGoals.push(goal);
-        return c.json({ goal });
+        return c.json(goal);
     });
