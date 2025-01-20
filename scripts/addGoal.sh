0f0db46a4d7649b03dc821d30863c4d7129b307f
Jakob Jónasson <jakob.jonasson@gmail.com>
2025-01-20 09:00:27 +0000

    Validate Goals (#3)

--- scripts/addGoal.sh
+++ scripts/addGoal.sh
@@ -1,3 +1,3 @@
 curl -X POST http://localhost:3000/api/goals \
 -H "Content-Type: application/json" \
--d '{"id": "4", "title": "Goal 4", "date": "2024-01-13", "description": "This is it", "completed": true}'
+-d '{"title": "Goal 4", "date": "2024-01-13", "description": "This is it", "completed": true}'
