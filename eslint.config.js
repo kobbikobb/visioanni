0f897469ecf6e67d7003d5c085bda6138b4e0103
Jakob Jónasson <jakob.jonasson@gmail.com>
2025-01-22 09:32:01 +0000

    Fix lint config (#5)

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,23 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import eslintConfigPrettier from "eslint-config-prettier";
-import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-    {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-    {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+    { languageOptions: { globals: {...globals.browser, ...globals.node } } },
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     pluginReact.configs.flat.recommended,
     eslintConfigPrettier,
-    eslintPluginPrettier
+    eslintPluginPrettier,
+    {
+        settings: {
+            react: {
+                version: 'detect'
+            }
+        }
+    }
 ];
