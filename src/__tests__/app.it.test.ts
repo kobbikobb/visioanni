50505178010caca5457c3f0b2793389dc45cf2c7
Jakob Jónasson <jakob.jonasson@gmail.com>
2025-01-22 10:18:22 +0000

    Delete goal (#6)

--- src/__tests__/app.it.test.ts
+++ src/__tests__/app.it.test.ts
@@ -1,22 +1,41 @@
 import { describe, it, expect } from 'bun:test';
 import app from '../app';
 
+const aGoal = () => {
+    return {
+        title: 'New Goal',
+        date: '2021-01-01',
+        completed: false
+    };
+};
+
+const getData = (path: string) => {
+    return app.request(path);
+};
+
+const postData = (path: string, obj: object) => {
+    return app.request(path, {
+        method: 'POST',
+        body: JSON.stringify(obj),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+};
+
+const deleteData = (path: string) => {
+    console.log('path', path);
+    return app.request(path, {
+        method: 'DELETE'
+    });
+};
+
 describe('Goals API', () => {
     it('should GET goals', async () => {
-        const newGoal = {
-            title: 'New Goal',
-            date: '2021-01-01',
-            completed: false
-        };
-        await app.request('/api/goals', {
-            method: 'POST',
-            body: JSON.stringify(newGoal),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
+        const newGoal = aGoal();
+        await postData('/api/goals', newGoal);
 
-        const goalsResponse = await app.request('/api/goals');
+        const goalsResponse = await getData('/api/goals');
 
         expect(goalsResponse.status).toBe(200);
         const goalsData = await goalsResponse.json();
@@ -26,21 +45,11 @@ describe('Goals API', () => {
     });
 
     it('should GET goal', async () => {
-        const newGoal = {
-            title: 'New Goal',
-            date: '2021-01-01',
-            completed: false
-        };
-        const newGoalResponse = await app.request('/api/goals', {
-            method: 'POST',
-            body: JSON.stringify(newGoal),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
+        const newGoal = aGoal();
+        const newGoalResponse = await postData('/api/goals', newGoal);
         const newGoalData = await newGoalResponse.json();
 
-        const goalResponse = await app.request(`/api/goals/${newGoalData.id}`);
+        const goalResponse = await getData(`/api/goals/${newGoalData.id}`);
 
         expect(goalResponse.status).toBe(200);
         const goalData = await goalResponse.json();
@@ -48,22 +57,25 @@ describe('Goals API', () => {
     });
 
     it('should POST a new goal', async () => {
-        const newGoal = {
-            title: 'New Goal',
-            date: '2021-01-01',
-            completed: false
-        };
-
-        const newGoalResponse = await app.request('/api/goals', {
-            method: 'POST',
-            body: JSON.stringify(newGoal),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
+        const newGoal = aGoal();
+        const newGoalResponse = await postData('/api/goals', newGoal);
 
         expect(newGoalResponse.status).toBe(201);
         const goalData = await newGoalResponse.json();
         expect(goalData).toMatchObject(newGoal);
+    });
+
+    it('should DELETE goal', async () => {
+        const newGoal = aGoal();
+        const newGoalResponse = await postData('/api/goals', newGoal);
+        const newGoalData = await newGoalResponse.json();
+
+        const deleteGoalResponse = await deleteData(
+            `/api/goals/${newGoalData.id}`
+        );
+
+        expect(deleteGoalResponse.status).toBe(204);
+        const goalResponse = await getData(`/api/goals/${newGoalData.id}`);
+        expect(goalResponse.status).toBe(404);
     });
 });
