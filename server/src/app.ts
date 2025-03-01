3a421c509231b3c7b6997dd42e7b4858459d5df7
Jakob Jonasson <jakob.jonasson@gmail.com>
2025-03-01 19:58:56 +0000

    Share types between backend and frontend

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,9 +11,11 @@ app.get('/test', (c) => {
     return c.json({ message: 'test result' });
 });
 
-app.route('/api/goals', goalsRoute);
+// eslint-disable-next-line
+const apiRoutes = app.basePath('/api').route('/goals', goalsRoute);
 
 app.get('*', serveStatic({ root: './frontend/dist' }));
 app.get('*', serveStatic({ path: './frontend/dist/index.html' }));
 
 export default app;
+export type ApiRoutes = typeof apiRoutes;
