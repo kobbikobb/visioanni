2167043db533d31f41267bb79072e37e4cbb8722
Jakob Jonasson <jakob.jonasson@gmail.com>
2025-03-05 09:37:03 +0000

    Add router

---
+++ frontend/src/routes/__root.tsx
@@ -0,0 +1,44 @@
+import React, { Suspense } from 'react';
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
+
+const TanStackRouterDevtools =
+    process.env.NODE_ENV === 'production'
+        ? () => null // Render nothing in production
+        : React.lazy(() =>
+              import('@tanstack/router-devtools').then((res) => ({
+                  default: res.TanStackRouterDevtools
+              }))
+          );
+
+const Nav = () => {
+    return (
+        <div className="p-2 flex gap-2">
+            <Link to="/" className="[&.active]:font-bold">
+                Home
+            </Link>{' '}
+            <Link to="/goals" className="[&.active]:font-bold">
+                Goals
+            </Link>
+            <Link to="/about" className="[&.active]:font-bold">
+                About
+            </Link>
+        </div>
+    );
+};
+
+const Root = () => {
+    return (
+        <>
+            <Nav />
+            <hr />
+            <Outlet />
+            <Suspense>
+                <TanStackRouterDevtools />
+            </Suspense>
+        </>
+    );
+};
+
+export const Route = createRootRoute({
+    component: Root
+});
