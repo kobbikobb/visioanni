5cf4b0de186acf43075ad8bd36a39cbd7b9745ff
Jakob Jonasson <jakob.jonasson@gmail.com>
2025-03-11 09:59:06 +0000

    Auth

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import { serveStatic } from 'hono/bun';
 import { goalsRoute } from './routes/goals';
+import { authRoute } from './routes/auth';
 
 const app = new Hono();
 
@@ -12,7 +13,10 @@ app.get('/test', (c) => {
 });
 
 // eslint-disable-next-line
-const apiRoutes = app.basePath('/api').route('/goals', goalsRoute);
+const apiRoutes = app
+    .basePath('/api')
+    .route('/goals', goalsRoute)
+    .route('/', authRoute);
 
 app.get('*', serveStatic({ root: './frontend/dist' }));
 app.get('*', serveStatic({ path: './frontend/dist/index.html' }));
