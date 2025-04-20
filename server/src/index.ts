c94739a02086c3ef2a2505891453ac9ff6fb1628
Jakob Jónasson <jakob.jonasson@gmail.com>
2025-04-20 10:06:55 +0000

    Add logging

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,20 @@
 import app from './app';
 
+const envVars = [
+    'KINDE_ISSUER_URL',
+    'KINDE_CLIENT_ID',
+    'KINDE_CLIENT_SECRET',
+    'KINDE_SITE_URL',
+    'KINDE_LOGOUT_REDIRECT_URI',
+    'KINDE_DOMAIN',
+    'KINDE_REDIRECT_URI',
+    'DATABASE_URL'
+];
+
+envVars.forEach((key) => {
+    console.log(`${key}=${process.env[key] || '(not set)'}`);
+});
+
 Bun.serve({
     fetch: app.fetch
 });
